d36107efb17dda3f3b8a6bb85c6d6bad7111982c
Boluwatife <Boluwatifeoladejo@gmail.com>
2024-03-29 21:00:36 +0100

    project scrabfold

---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import MainBody from "./MainBody";
+
+export {
+  CTA,
+  Footer,
+  Header,
+  Hero,
+  MainBody
+}
