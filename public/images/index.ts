55cb54d29b026c184a3c45aa805d520ffdfbe41e
Boluwatife <Boluwatifeoladejo@gmail.com>
2024-05-10 01:23:54 +0100

    fixing production bug

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,1 +1,39 @@
+import account from './Account.png';
+import apphome from './app-home.png';
+import bank from './bank.png';
+import bannerImage from './BannerImage.png';
+import budget from './Budget.png';
+import chat from './Chatt.png'
+import logo from './logo.png';
+import moneyStack from './money-stack.png';
+import newNotification from './New-Notification.png';
+import Notification from './Notification.png';
+import notify from './notify.png';
+import rectangle from './Rectangle.png'
+import googlePlay from './google-play.png'
+import cardIcon from './card-icon.png'
+import Analysis from './Analysis.png'
+import track from './Track-pic.png'
+import budg from './Analysis_.png'
+import doe from './doe.png'
 
+export {
+  account,
+  apphome,
+  bank,
+  bannerImage,
+  budget,
+  chat,
+  logo,
+  moneyStack,
+  newNotification,
+  Notification,
+  notify,
+  rectangle,
+  googlePlay,
+  cardIcon,
+  Analysis,
+  track,
+  budg,
+  doe
+}
