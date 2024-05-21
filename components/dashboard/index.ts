d98d389026c11df309f29b9b1e5267c5e85a7965
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2024-05-21 23:02:01 +0100

    ADDED : Subscription Page

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -4,6 +4,14 @@ import DashboardLayout from './dashboardLayout';
 import DashboardPage from './pages/DashboardPage';
 import NotificationPage from './pages/NotificationPage';
 import SubscriptionPage from './pages/SubscriptionPage';
+import UserSubscriptionDetails from "./UserSubscriptionDetails";
+import UserSubscriptionStats from "./UserSubscriptionStats";
+import UserubscriptionGraph from "./UserSubscriptionGraph";
+import ButtonComponent from "./ButtonComponent";
+import UserSubscriptionLineGraph from './UserSubscriptionLineGraph'
+import UserSubscriptionPieChart from './UserSubscriptionPieChart'
+import IncreaseIcon from "@/public/images/icons/IncreaseIcon";
+import DecreaseIcon from "@/public/images/icons/DecreaseIcon";
 
 
 
@@ -13,5 +21,13 @@ export {
   DashboardLayout,
   DashboardPage,
   NotificationPage,
-  SubscriptionPage
+  SubscriptionPage,
+  UserSubscriptionDetails,
+  UserSubscriptionStats, 
+  UserubscriptionGraph,
+  ButtonComponent,
+  UserSubscriptionLineGraph,
+  UserSubscriptionPieChart,
+  IncreaseIcon,
+  DecreaseIcon
 }
